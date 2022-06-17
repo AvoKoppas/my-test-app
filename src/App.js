33a1afd14fbd9461255071b5b4d814366d204c40
AvoKoppas <avo.okas@live.com>
2022-06-17 23:31:24 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const questions = [
+        {
+            questionText: 'Mis on Eesti pindala?',
+            answerOptions: [
+                {answerText: 12000, isCorrect: false},
+                {answerText: 34500, isCorrect: false},
+                {answerText: 44500, isCorrect: true},
+                {answerText: 51000, isCorrect: false}
+            ],
+        },
+        {
+            questionText: 'Mis on Eesti pealinn?',
+            answerOptions: [
+                {answerText: 'Tallinn', isCorrect: true},
+                {answerText: 'Narva', isCorrect: false},
+                {answerText: 'Tartu', isCorrect: false},
+                {answerText: 'Pärnu', isCorrect: false}
+            ]
+        }
+    ];
+
+    const [currentQuestion, setCurrentQuestion] = useState(0)
+    const [score, setScore] = useState(0)
+    const [showScore, setShowScore] = useState(false)
+
+    const handleAnswerButtonClick = (isCorrect) => {
+        if (isCorrect === true) {
+            setScore(score + 1)
+        }
+        const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questions.length) {
+            setCurrentQuestion(nextQuestion)
+        } else {
+            setShowScore(true);
+        }
+    }
+
+    return (<div className="app">
+            {showScore ? (<div className="score-section">Sa
+                skoorisid {score} punkti {questions.length}-st </div>) : (
+                <>
+                    <div className='question-section'>
+                        <div className='question-count'>
+                            <span>Küsimus nr {currentQuestion + 1}</span>/{questions.length}
+                        </div>
+                        <div
+                            className='question-text'>{questions[currentQuestion].questionText}</div>
+                    </div>
+                    <div className='answer-section'>
+                        {questions[currentQuestion].answerOptions.map((answerOption) =>
+                            <button
+                                onClick={() => handleAnswerButtonClick(answerOption.isCorrect)}>{answerOption.answerText}</button>)}
+                    </div>
+                </>
+            )}
+        </div>
+    )
+        ;
 }
 
 export default App;
